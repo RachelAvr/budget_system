77d14ca94e41bc32b7576983046a917ee64e576b
Rachel Avraham <rachel426@gmail.com>
2025-08-30 10:42:14 +0300

    updated Adi help

--- src/AddNewExpense.jsx
+++ src/AddNewExpense.jsx
@@ -1,27 +1,28 @@
 import {useState,useEffect} from 'react';
 
 export default function AddNewExpense({onAdd}){
-    const [category ,setCategory]= useState('');
-    const [planned, setPlanned] = useState(0);
-    const [actual, setActual] = useState(0);
+    const [Category ,setCategory]= useState('');
+    const [Planned, setPlanned] = useState(0);
+    const [Actual, setActual] = useState(0);
     //*You’re storing numbers as strings in state (useState(''))*//
     
       useEffect(() => {
         console.log('[addNewExpense] component mounted');}, []);
 
     
-    //*the form allows submitting empty values. Add required attributes or guard before onAdd*//
+    //*the form allows submitting empty values. Add required attributes or guard before onAdd yes! but i want to keep the option to have planned/actul=0*//
     const handleSubmit = (e)=>{
             console.log('[addNewExpense] user clicked add')
             e.preventDefault()
-            if (!category || !planned || !actual) {
+            if (!Category || (!Planned && !Actual)) {
             alert("Please fill out all fields");
+            console.log('[addNewExpense] cannot add without ctegory and or cost')
             return;
     }
-            //*You’re mixing lowercase (planned) and uppercase (Planned)*//
-            onAdd({ category, planned: Number(planned), actual: Number(actual) });
+            //*You’re mixing lowercase (planned) and uppercase (Planned) yes! but notice that the table is a specifc packge*//
+            onAdd({ Category:Category, Planned:Planned||0, Actual:Actual||0 });
             //*was instead of wad*//
-            console.log('[addNewExpense] a row was added to the table', [category, planned, actual])
+            console.log('[addNewExpense] a row was added to the table', [Category, Planned, Actual])
             setCategory('');
             setPlanned('');
             setActual('');
@@ -30,7 +31,7 @@ export default function AddNewExpense({onAdd}){
     return(
         <form onSubmit={handleSubmit}>
                  <label>Pick a category:
-                <select name="selectCategory" value={category}  onChange={(e)=>{setCategory(e.target.value); console.log('user selected category',e.target.value ) }}>
+                <select name="selectCategory" value={Category}  onChange={(e)=>{setCategory(e.target.value); console.log('user selected category',e.target.value ) }}>
                     <option value="" disabled hidden></option>
                     <option value="Rent">Rent</option>
                     <option value="Bills">Bills</option>
@@ -39,8 +40,8 @@ export default function AddNewExpense({onAdd}){
                     <option value="Car">Car</option>
                 </select>
                 </label>
-                <input type="number" placeholder="Planned cost" value={planned} onChange={(e) => setPlanned(Number(e.target.value))};console.log('user typed planned cost', e.target.value)}}></input>
-                <input type="number" placeholder="Actual cost" value={actual} onChange={(e)=> setActual(Number(e.target.value))};console.log('user typed planned cost', e.target.value)}}></input>
+                <input type="number" min="0" placeholder="Planned cost" value={Planned} onChange={(e) => {setPlanned(Number(e.target.value));console.log('user typed planned cost', e.target.value)}}/>
+                <input type="number" min="0" placeholder="Actual cost" value={Actual} onChange={(e)=> {setActual(Number(e.target.value));console.log('user typed planned cost', e.target.value)}}/>
                 <button type="submit">Add</button>
 
 
